f15155f3f36b6dcde8e04a386f851a935c8c0f2b
renaldybrada <renaldybrada@gmail.com>
2024-06-19 14:02:49 +0700

    fixing password hash

--- src/database/factories/users.factory.ts
+++ src/database/factories/users.factory.ts
@@ -1,12 +1,14 @@
 import { define } from 'typeorm-seeding';
 import { faker } from '@faker-js/faker';
 import { Users } from '../entities/users/users.entity';
+import * as bcrypt from 'bcrypt';
 
 define(Users, () => {
-  const user = new Users();
-  user.name = 'admin';
-  user.email = 'admin@mail.com';
-  user.password = 'rahasia123';
-  user.isAdmin = true;
-  return user;
+    const user = new Users();
+
+    user.name = 'admin';
+    user.email = 'admin@mail.com';
+    user.password = '$2b$10$YdXsACacWvrg8BKek4HeG.PwIaVUvPyHS8GsjcXcrqetd97nmgC9G' // rahasia123
+    user.isAdmin = true;
+    return user;
 });
